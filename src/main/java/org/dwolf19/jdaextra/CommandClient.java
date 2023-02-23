269a6cd7fd5ae0040b334fc0c88b289f19e0ab71
DWolf_19 <leoconnect.o@gmail.com>
2023-02-23 16:56:14 +0300

    Add prefix command detection

--- src/main/java/org/dwolf19/jdaextra/CommandClient.java
+++ src/main/java/org/dwolf19/jdaextra/CommandClient.java
@@ -1,5 +1,7 @@
 package org.dwolf19.jdaextra;
 
+import org.dwolf19.jdaextra.events.CommandEvent;
+
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.interaction.command.MessageContextInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -7,14 +9,18 @@ import net.dv8tion.jda.api.events.interaction.command.UserContextInteractionEven
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 
+import java.util.Optional;
+
 import org.jetbrains.annotations.NotNull;
 
 public class CommandClient implements EventListener {
 
     protected String prefix;
+    protected boolean useMention;
 
-    CommandClient(String prefix) {
+    CommandClient(String prefix, boolean useMention) {
         this.prefix = prefix;
+        this.useMention = useMention;
     }
 
     @Override
@@ -33,9 +39,59 @@ public class CommandClient implements EventListener {
     }
 
     private void onMessageReceivedEvent(MessageReceivedEvent event) {
+        Optional<PrefixCommandStructure> command = Optional.ofNullable(parseMessage(event));
+
+        // If the message is not a command
+        if (command.isEmpty())
+            return;
+    }
+
+    private PrefixCommandStructure parseMessage(MessageReceivedEvent event) {
+        String rawContent = event.getMessage().getContentRaw();
+        PrefixCommandStructure command = new PrefixCommandStructure();
+
+        command.setPrefix(prefix);
+
+        if (rawContent.startsWith(prefix)) {
+            for (int symbol = prefix.length(); symbol < rawContent.length(); symbol++)
+                if (Character.isWhitespace(rawContent.charAt(symbol))) {
+                    command.setName(rawContent.substring(prefix.length(), symbol));
+                    break;
+                }
+        } else
+            return null;
+
+        return command;
+    }
+
+    private static class PrefixCommandStructure {
+        String prefix;
+        String name;
+
+        PrefixCommandStructure setPrefix(String prefix) {
+            this.prefix = prefix;
+
+            return this;
+        }
+
+        String getPrefix() {
+            return this.prefix;
+        }
+
+        PrefixCommandStructure setName(String name) {
+            this.name = name;
+
+            return this;
+        }
+
+        String getName() {
+            return this.name;
+        }
+
     }
 
     private void onSlashCommandInteractionEvent(SlashCommandInteractionEvent event) {
+
     }
 
     private void onUserContextInteractionEvent(UserContextInteractionEvent event) {
