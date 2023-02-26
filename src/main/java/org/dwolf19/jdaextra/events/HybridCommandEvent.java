06969453dda02e6252420979a0dd66e1385676d4
DWolf_19 <leoconnect.o@gmail.com>
2023-02-26 23:31:54 +0300

    Add a generic to hybrid command event

--- src/main/java/org/dwolf19/jdaextra/events/HybridCommandEvent.java
+++ src/main/java/org/dwolf19/jdaextra/events/HybridCommandEvent.java
@@ -2,24 +2,27 @@ package org.dwolf19.jdaextra.events;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.events.Event;
+import net.dv8tion.jda.api.requests.RestAction;
+import net.dv8tion.jda.api.utils.messages.MessageCreateData;
+
 import org.dwolf19.jdaextra.CommandClient;
 
-public abstract class HybridCommandEvent {
+public abstract class HybridCommandEvent<E extends Event> {
 
-    private final Event event;
-    private final CommandClient client;
+    private final E event;
 
-    public HybridCommandEvent(Event event, CommandClient client) {
+    public HybridCommandEvent(E event) {
         this.event = event;
-        this.client = client;
     }
 
-    private Event getSourceEvent() {
+    public E getSourceEvent() {
         return event;
     }
 
+    // --- Wrappers ---
+
     public CommandClient getClient() {
-        return client;
+        return event.getClient();
     }
 
     public JDA getJDA() {
