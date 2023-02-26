fe804fe080a39b1573c3996da9babb63a345c0f1
DWolf_19 <leoconnect.o@gmail.com>
2023-02-26 13:04:34 +0300

    Add the ability to get client from slash command

--- src/main/java/org/dwolf19/jdaextra/events/SlashCommandEvent.java
+++ src/main/java/org/dwolf19/jdaextra/events/SlashCommandEvent.java
@@ -2,10 +2,20 @@ package org.dwolf19.jdaextra.events;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
+import org.dwolf19.jdaextra.CommandClient;
+
 public class SlashCommandEvent extends SlashCommandInteractionEvent {
 
-    public SlashCommandEvent(SlashCommandInteractionEvent event) {
+    private final CommandClient client;
+
+    public SlashCommandEvent(SlashCommandInteractionEvent event, CommandClient client) {
         super(event.getJDA(), event.getResponseNumber(), event);
+
+        this.client = client;
+    }
+
+    public CommandClient getClient() {
+        return client;
     }
 
 }
