ffbba741c5acc009f58a6208b7eece0870b962ae
DWolf_19 <leoconnect.o@gmail.com>
2023-02-26 23:18:37 +0300

    Add an abstract class for hybrid commands

---
+++ src/main/java/org/dwolf19/jdaextra/commands/HybridCommand.java
@@ -0,0 +1,9 @@
+package org.dwolf19.jdaextra.commands;
+
+import org.dwolf19.jdaextra.events.HybridCommandEvent;
+
+public abstract class HybridCommand extends Command {
+
+    public abstract void logic(HybridCommandEvent event);
+
+}
