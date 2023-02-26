4e487820258bcbf86b448f1ec291921066ebd657
DWolf_19 <leoconnect.o@gmail.com>
2023-02-27 00:24:41 +0300

    Implement some wrapper methods

--- src/main/java/org/dwolf19/jdaextra/events/HybridCommandEvent.java
+++ src/main/java/org/dwolf19/jdaextra/events/HybridCommandEvent.java
@@ -1,6 +1,10 @@
 package org.dwolf19.jdaextra.events;
 
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.channel.ChannelType;
+import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
 import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.requests.RestAction;
 import net.dv8tion.jda.api.utils.messages.MessageCreateData;
@@ -27,6 +31,34 @@ public abstract class HybridCommandEvent<E extends Event> {
 
     public JDA getJDA() {
         return event.getJDA();
+    }
+
+    public Guild getGuild() {
+        return event.getGuild();
+    }
+
+    public MessageChannelUnion getChannel() {
+        return event.getChannel();
+    }
+
+    public Member getMember() {
+        return event.getMember();
+    }
+
+    public String getName() {
+        return event.getName();
+    }
+
+    public ChannelType getChannelType() {
+        return event.getChannelType();
+    }
+
+    public RestAction<?> reply(String content) {
+        return event.reply(content);
+    }
+
+    public RestAction<?> reply(MessageCreateData message) {
+        return event.reply(message);
     }
 
 }
