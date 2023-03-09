ff55587726e8f940bd7400b0b0a54a12f57503f2
DWolf Nineteen <leoconnect.o@gmail.com>
2023-03-09 18:14:03 +0300

    Fix wildcard in hybrid command

--- src/main/java/org/dwolf19/jdaextra/commands/HybridCommand.java
+++ src/main/java/org/dwolf19/jdaextra/commands/HybridCommand.java
@@ -4,6 +4,6 @@ import org.dwolf19.jdaextra.events.HybridCommandEvent;
 
 public abstract class HybridCommand extends Command {
 
-    public abstract void logic(HybridCommandEvent event);
+    public abstract void logic(HybridCommandEvent<?> event);
 
 }
