a0723a57b7e0a41dc5eefcb1b6ec2637e2494b04
DWolf Nineteen <dwolfnineteen@gmail.com>
2023-06-19 13:20:43 +0300

    Move CommandClient to ListenerAdapter

--- src/main/java/org/dwolf19/jdaextra/CommandClient.java
+++ src/main/java/org/dwolf19/jdaextra/CommandClient.java
@@ -1,35 +1,25 @@
 package org.dwolf19.jdaextra;
 
-import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
-import net.dv8tion.jda.api.hooks.EventListener;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import org.jetbrains.annotations.NotNull;
 
-public class CommandClient implements EventListener {
+public class CommandClient extends ListenerAdapter {
     @Override
-    public void onEvent(@NotNull GenericEvent event) {
-        if (event instanceof ReadyEvent)
-            onReadyEvent((ReadyEvent) event);
-
-        else if (event instanceof MessageReceivedEvent)
-            onMessageReceivedEvent((MessageReceivedEvent) event);
-
-        else if (event instanceof SlashCommandInteractionEvent)
-            onSlashCommandInteractionEvent((SlashCommandInteractionEvent) event);
-    }
-
-    private void onReadyEvent(@NotNull ReadyEvent event) {
+    public void onReady(@NotNull ReadyEvent event) {
         // TODO: add logic
     }
 
-    private void onMessageReceivedEvent(@NotNull MessageReceivedEvent event) {
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         // TODO: add logic
     }
 
-    private void onSlashCommandInteractionEvent(@NotNull SlashCommandInteractionEvent event) {
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         // TODO: add logic
     }
 }
