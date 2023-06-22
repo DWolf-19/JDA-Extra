b7a471fb42bd5e82bac00ccf0271bb8ef26d181e
DWolf Nineteen <dwolfnineteen@gmail.com>
2023-06-22 22:22:27 +0300

    Add Command abstract base class

---
+++ lib/src/main/java/org/dwolf19/jdaextra/commands/Command.java
@@ -0,0 +1,33 @@
+package org.dwolf19.jdaextra.commands;
+
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
+
+public abstract class Command {
+    protected String name;
+    protected String description;
+    protected DefaultMemberPermissions defaultMemberPermissions;
+    protected boolean guildOnly;
+    protected boolean nsfw;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public DefaultMemberPermissions getDefaultMemberPermissions() {
+        return defaultMemberPermissions;
+    }
+
+    public boolean isGuildOnly() {
+        return guildOnly;
+    }
+
+    public boolean isNsfw() {
+        return nsfw;
+    }
+
+    public abstract void executeLogic();
+}
