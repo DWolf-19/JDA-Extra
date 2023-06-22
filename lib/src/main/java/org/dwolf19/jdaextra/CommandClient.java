ee99866e635a9ac7833f15adf64a3926b2392955
DWolf Nineteen <dwolfnineteen@gmail.com>
2023-06-23 00:08:19 +0300

    Setup CommandClient build with prefix setting

--- lib/src/main/java/org/dwolf19/jdaextra/CommandClient.java
+++ lib/src/main/java/org/dwolf19/jdaextra/CommandClient.java
@@ -5,9 +5,35 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import org.dwolf19.jdaextra.commands.HybridCommand;
+import org.dwolf19.jdaextra.commands.PrefixCommand;
+import org.dwolf19.jdaextra.commands.SlashCommand;
+
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+
 public class CommandClient extends ListenerAdapter {
+    private final String prefix;
+    private final boolean whenMention;
+
+    private final HashMap<String, HybridCommand> hybridCommands;
+    private final HashMap<String, PrefixCommand> prefixCommands;
+    private final HashMap<String, SlashCommand> slashCommands;
+
+    public CommandClient(@NotNull String prefix,
+                         boolean whenMention,
+                         @NotNull HashMap<String, HybridCommand> hybridCommands,
+                         @NotNull HashMap<String, PrefixCommand> prefixCommands,
+                         @NotNull HashMap<String, SlashCommand> slashCommands) {
+        this.prefix = prefix;
+        this.whenMention = whenMention;
+
+        this.hybridCommands = hybridCommands;
+        this.prefixCommands = prefixCommands;
+        this.slashCommands = slashCommands;
+    }
+
     @Override
     public void onReady(@NotNull ReadyEvent event) {
         // TODO: add logic
--- lib/src/main/java/org/dwolf19/jdaextra/CommandClientBuilder.java
+++ lib/src/main/java/org/dwolf19/jdaextra/CommandClientBuilder.java
@@ -1,9 +1,67 @@
 package org.dwolf19.jdaextra;
 
+import org.dwolf19.jdaextra.commands.HybridCommand;
+import org.dwolf19.jdaextra.commands.PrefixCommand;
+import org.dwolf19.jdaextra.commands.SlashCommand;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+
 public class CommandClientBuilder {
-    public CommandClient build() {
-        return new CommandClient();
+    private String prefix;
+    private boolean whenMention = false;
+
+    private final HashMap<String, HybridCommand> hybridCommands = new HashMap<>();
+    private final HashMap<String, PrefixCommand> prefixCommands = new HashMap<>();
+    private final HashMap<String, SlashCommand> slashCommands = new HashMap<>();
+
+    @NotNull
+    public CommandClientBuilder setPrefix(@NotNull String prefix) {
+        this.prefix = prefix;
+
+        return this;
     }
 
-    // TODO: add methods for adding commands and configuring command client
+    @NotNull
+    public CommandClientBuilder setWhenMentionOr(@NotNull String prefix) {
+        this.prefix = prefix;
+        whenMention = true;
+
+        return this;
+    }
+
+    @NotNull
+    public CommandClientBuilder addHybridCommands(@NotNull HybridCommand... commands) {
+        for (HybridCommand command : commands)
+            hybridCommands.put(command.getName(), command);
+
+        return this;
+    }
+
+    @NotNull
+    public CommandClientBuilder addPrefixCommands(@NotNull PrefixCommand... commands) {
+        for (PrefixCommand command : commands)
+            prefixCommands.put(command.getName(), command);
+
+        return this;
+    }
+
+    @NotNull
+    public CommandClientBuilder addSlashCommands(@NotNull SlashCommand... commands) {
+        for (SlashCommand command : commands)
+            slashCommands.put(command.getName(), command);
+
+        return this;
+    }
+
+    @NotNull
+    public CommandClient build() {
+        return new CommandClient(
+                prefix,
+                whenMention,
+                hybridCommands,
+                prefixCommands,
+                slashCommands);
+    }
 }
