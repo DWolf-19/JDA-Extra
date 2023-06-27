8be9d9ec7a33c0f936ec926931a3f0dda2bbe42c
DWolf Nineteen <dwolfnineteen@gmail.com>
2023-06-27 12:44:49 +0300

    Add PrefixCommandModel
    This class is built by the parser as a representation of what user sent

---
+++ lib/src/main/java/org/dwolf19/jdaextra/models/PrefixCommandModel.java
@@ -0,0 +1,38 @@
+package org.dwolf19.jdaextra.models;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class PrefixCommandModel {
+    private String prefix;
+    private String name;
+    private ArrayList<String> args;
+
+    @NotNull
+    public String getPrefix() {
+        return prefix;
+    }
+
+    @NotNull
+    public String getName() {
+        return name;
+    }
+
+    @NotNull
+    public ArrayList<String> getArgs() {
+        return args;
+    }
+
+    public void setPrefix(@NotNull String prefix) {
+        this.prefix = prefix;
+    }
+
+    public void setName(@NotNull String name) {
+        this.name = name;
+    }
+
+    public void setArgs(@NotNull ArrayList<String> args) {
+        this.args = args;
+    }
+}
