6c2778bf47a6c40a00ce3a6e93d8c3d1702e7ac0
DWolf Nineteen <dwolfnineteen@gmail.com>
2023-07-11 12:07:02 +0300

    Cleanup command classes

--- lib/src/main/java/org/dwolf19/jdaextra/commands/HybridCommand.java
+++ lib/src/main/java/org/dwolf19/jdaextra/commands/HybridCommand.java
@@ -1,9 +1,4 @@
 package org.dwolf19.jdaextra.commands;
 
-import org.dwolf19.jdaextra.events.HybridCommandEvent;
-
 public abstract class HybridCommand extends Command {
-    public abstract void executeHybridLogic(HybridCommandEvent event);
-
-    // TODO: add logic
 }
