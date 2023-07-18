c3e51b8b4d8de073e0f7a06408cb1a4164e9a207
DWolf Nineteen <dwolfnineteen@gmail.com>
2023-07-18 16:07:03 +0300

    Implement and refactoring command builders

--- lib/src/main/java/org/dwolf19/jdaextra/builders/HybridCommandBuilder.java
+++ lib/src/main/java/org/dwolf19/jdaextra/builders/HybridCommandBuilder.java
@@ -1,14 +1,17 @@
 package org.dwolf19.jdaextra.builders;
 
+import org.dwolf19.jdaextra.annotations.ExtraHybridCommand;
+import org.dwolf19.jdaextra.annotations.ExtraMainCommand;
+import org.dwolf19.jdaextra.commands.Command;
 import org.dwolf19.jdaextra.commands.HybridCommand;
+import org.dwolf19.jdaextra.exceptions.CommandAnnotationNotFoundException;
 import org.dwolf19.jdaextra.models.HybridCommandModel;
-
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class HybridCommandBuilder implements CommandBuilder {
-    private final HybridCommand command;
+import java.lang.reflect.Method;
 
+public class HybridCommandBuilder extends CommandBuilder {
     public HybridCommandBuilder(@NotNull HybridCommand command) {
         this.command = command;
     }
@@ -17,8 +20,27 @@ public class HybridCommandBuilder implements CommandBuilder {
     @Nullable
     public HybridCommandModel buildModel() {
         HybridCommandModel model = new HybridCommandModel();
+        Class<? extends Command> commandClass = command.getClass();
+
+        model.setCommand(command);
+
+        for (Method method : command.getClass().getDeclaredMethods()) {
+            if (method.isAnnotationPresent(ExtraMainCommand.class)) {
+                model.setMain(method);
+
+                break;
+            }
+        }
+
+        if (commandClass.isAnnotationPresent(ExtraHybridCommand.class)) {
+            ExtraHybridCommand classAnnotation = commandClass.getAnnotation(ExtraHybridCommand.class);
+
+            model.setName(classAnnotation.name().isEmpty() ? model.getMain().getName() : classAnnotation.name());
+            model.setDescription(classAnnotation.description());
 
         // TODO: add logic
+        } else
+            throw new CommandAnnotationNotFoundException();
 
         return model;
     }
