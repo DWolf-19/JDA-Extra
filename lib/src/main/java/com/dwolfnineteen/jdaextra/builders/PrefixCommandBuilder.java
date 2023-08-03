84261f89eabf84a18af4160a6c316ca02292a91d
DWolf Nineteen <dwolfnineteen@gmail.com>
2023-08-03 23:06:27 +0300

    Take out the entry point build to CommandBuilder

--- lib/src/main/java/com/dwolfnineteen/jdaextra/builders/PrefixCommandBuilder.java
+++ lib/src/main/java/com/dwolfnineteen/jdaextra/builders/PrefixCommandBuilder.java
@@ -21,7 +21,6 @@ SOFTWARE.
 */
 package com.dwolfnineteen.jdaextra.builders;
 
-import com.dwolfnineteen.jdaextra.annotations.ExtraMainCommand;
 import com.dwolfnineteen.jdaextra.annotations.ExtraPrefixCommand;
 import com.dwolfnineteen.jdaextra.commands.PrefixCommand;
 import com.dwolfnineteen.jdaextra.commands.Command;
@@ -29,8 +28,6 @@ import com.dwolfnineteen.jdaextra.exceptions.CommandAnnotationNotFoundException;
 import com.dwolfnineteen.jdaextra.models.PrefixCommandModel;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.lang.reflect.Method;
 
 public class PrefixCommandBuilder extends CommandBuilder {
     public PrefixCommandBuilder(@NotNull PrefixCommand command) {
@@ -44,14 +41,7 @@ public class PrefixCommandBuilder extends CommandBuilder {
         Class<? extends Command> commandClass = command.getClass();
 
         model.setCommand(command);
-
-        for (Method method : commandClass.getDeclaredMethods()) {
-            if (method.isAnnotationPresent(ExtraMainCommand.class)) {
-                model.setMain(method);
-
-                break;
-            }
-        }
+        model.setMain(buildMain());
 
         if (commandClass.isAnnotationPresent(ExtraPrefixCommand.class)) {
             ExtraPrefixCommand classAnnotation = commandClass.getAnnotation(ExtraPrefixCommand.class);
