8af6d5a2853168a8ac092dec89886a763af5812c
DWolf Nineteen <dwolfnineteen@gmail.com>
2023-10-17 18:38:12 +0300

    Add custom option mappings

---
+++ lib/src/main/java/com/dwolfnineteen/jdaextra/options/mappings/HybridOptionMapping.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2023 DWolf Nineteen & The JDA-Extra contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.dwolfnineteen.jdaextra.options.mappings;
+
+import net.dv8tion.jda.api.entities.IMentionable;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.ChannelType;
+import net.dv8tion.jda.api.entities.channel.unions.GuildChannelUnion;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import org.jetbrains.annotations.NotNull;
+
+public class HybridOptionMapping implements CommandOptionMapping {
+    private final CommandOptionMapping mapping;
+
+    public HybridOptionMapping(@NotNull CommandOptionMapping mapping) {
+        this.mapping = mapping;
+    }
+
+    @Override
+    public OptionType getType() {
+        return mapping.getType();
+    }
+
+    @Override
+    public String getName() {
+        return mapping.getName();
+    }
+
+    @Override
+    public String getAsString() {
+        return mapping.getAsString();
+    }
+
+    @Override
+    public boolean getAsBoolean() {
+        return mapping.getAsBoolean();
+    }
+
+    @Override
+    public long getAsLong() {
+        return mapping.getAsLong();
+    }
+
+    @Override
+    public int getAsInt() {
+        return mapping.getAsInt();
+    }
+
+    @Override
+    public double getAsDouble() {
+        return mapping.getAsDouble();
+    }
+
+    @Override
+    public IMentionable getAsMentionable() {
+        return mapping.getAsMentionable();
+    }
+
+    @Override
+    public Member getAsMember() {
+        return mapping.getAsMember();
+    }
+
+    @Override
+    public User getAsUser() {
+        return mapping.getAsUser();
+    }
+
+    @Override
+    public Role getAsRole() {
+        return mapping.getAsRole();
+    }
+
+    @Override
+    public GuildChannelUnion getAsChannel() {
+        return mapping.getAsChannel();
+    }
+
+    @Override
+    public ChannelType getChannelType() {
+        return mapping.getChannelType();
+    }
+}
---
+++ lib/src/main/java/com/dwolfnineteen/jdaextra/options/mappings/SlashOptionMapping.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2023 DWolf Nineteen & The JDA-Extra contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.dwolfnineteen.jdaextra.options.mappings;
+
+import net.dv8tion.jda.api.entities.IMentionable;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.ChannelType;
+import net.dv8tion.jda.api.entities.channel.unions.GuildChannelUnion;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+// This is for semantics!
+// TODO: Add OptionType checks to getters
+public class SlashOptionMapping implements CommandOptionMapping {
+    private final OptionMapping mapping;
+
+    public SlashOptionMapping(@NotNull OptionMapping mapping) {
+        this.mapping = mapping;
+    }
+
+    @Override
+    @NotNull
+    public OptionType getType() {
+        return mapping.getType();
+    }
+
+    @Override
+    @NotNull
+    public String getName() {
+        return mapping.getName();
+    }
+
+    @Override
+    @NotNull
+    public String getAsString() {
+        return mapping.getAsString();
+    }
+
+    @NotNull
+    public Message.Attachment getAsAttachment() {
+        return mapping.getAsAttachment();
+    }
+
+    @Override
+    public boolean getAsBoolean() {
+        return mapping.getAsBoolean();
+    }
+
+    @Override
+    public long getAsLong() {
+        return mapping.getAsLong();
+    }
+
+    @Override
+    public int getAsInt() {
+        return mapping.getAsInt();
+    }
+
+    @Override
+    public double getAsDouble() {
+        return mapping.getAsDouble();
+    }
+
+    @Override
+    @NotNull
+    public IMentionable getAsMentionable() {
+        return mapping.getAsMentionable();
+    }
+
+    @Override
+    @Nullable
+    public Member getAsMember() {
+        return mapping.getAsMember();
+    }
+
+    @Override
+    @NotNull
+    public User getAsUser() {
+        return mapping.getAsUser();
+    }
+
+    @Override
+    @NotNull
+    public Role getAsRole() {
+        return mapping.getAsRole();
+    }
+
+    @Override
+    @NotNull
+    public GuildChannelUnion getAsChannel() {
+        return mapping.getAsChannel();
+    }
+
+    @Override
+    @NotNull
+    public ChannelType getChannelType() {
+        return mapping.getChannelType();
+    }
+}
