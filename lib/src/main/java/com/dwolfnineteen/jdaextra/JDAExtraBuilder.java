8312aff796cde7f69cc51293ee6ef06023582374
DWolf Nineteen <dwolfnineteen@gmail.com>
2023-10-20 14:06:03 +0300

    Code style fixes (#109)
    ### Changes
    - ❌ Internal
    - ❌ Interface (affects end-user code)
    - ❌ Gradle (wrapper, scripts, dependencies)
    - ❌ Docs
    - ❌ Metadata (README, copyright, Git files, files in `.github`)
    - ✅ Other: Code refactoring
    ### Description
    Fixes related to copyright headers and Google Java Code Style. Code
    cleanup.

--- lib/src/main/java/com/dwolfnineteen/jdaextra/JDAExtraBuilder.java
+++ lib/src/main/java/com/dwolfnineteen/jdaextra/JDAExtraBuilder.java
@@ -1,24 +1,24 @@
 /*
-Copyright (c) 2023 DWolf Nineteen & The JDA-Extra contributors
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+ * Copyright (c) 2023 DWolf Nineteen & The JDA-Extra contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.dwolfnineteen.jdaextra;
 
 import com.dwolfnineteen.jdaextra.commands.HybridCommand;
@@ -28,14 +28,23 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class JDAExtraBuilder {
     private String prefix;
-    private boolean whenMention = false;
+    private boolean whenMention;
 
-    private final ArrayList<HybridCommand> hybridCommands = new ArrayList<>();
-    private final ArrayList<PrefixCommand> prefixCommands = new ArrayList<>();
-    private final ArrayList<SlashCommand> slashCommands = new ArrayList<>();
+    private final List<HybridCommand> hybridCommands;
+    private final List<PrefixCommand> prefixCommands;
+    private final List<SlashCommand> slashCommands;
+
+    {
+        whenMention = false;
+
+        hybridCommands = new ArrayList<>();
+        prefixCommands = new ArrayList<>();
+        slashCommands = new ArrayList<>();
+    }
 
     @NotNull
     public JDAExtraBuilder setPrefix(@NotNull String prefix) {
