d17c0209fd4c6ab7f5b162755a336cea5125232b
DWolf Nineteen <dwolfnineteen@gmail.com>
2024-03-24 21:26:23 +0300

    Move setters implementation to inherited classes (CommandModel)

--- lib/src/main/java/com/dwolfnineteen/jdaextra/models/SlashCommandModel.java
+++ lib/src/main/java/com/dwolfnineteen/jdaextra/models/SlashCommandModel.java
@@ -21,11 +21,18 @@
  */
 package com.dwolfnineteen.jdaextra.models;
 
+import com.dwolfnineteen.jdaextra.commands.BaseCommand;
 import com.dwolfnineteen.jdaextra.options.data.SlashOptionData;
 import net.dv8tion.jda.api.interactions.DiscordLocale;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandGroupData;
 import net.dv8tion.jda.api.interactions.commands.localization.LocalizationFunction;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -43,8 +50,7 @@ public class SlashCommandModel extends SlashLikeCommandModel {
      *
      * @return The description.
      */
-    @NotNull
-    public String getDescription() {
+    public @NotNull String getDescription() {
         return description;
     }
 
@@ -53,20 +59,18 @@ public class SlashCommandModel extends SlashLikeCommandModel {
      *
      * @return The command options.
      */
-    @NotNull
-    public List<SlashOptionData> getOptions() {
+    public @NotNull List<SlashOptionData> getOptions() {
         return options;
     }
 
     /**
-     * Sets multiple localizations of the command name.
+     * {@inheritDoc}
      *
-     * @param nameLocalizations {@link Map} of {@link DiscordLocale} and name on different languages.
+     * @param nameLocalizations {@inheritDoc}
      * @return The {@link SlashCommandModel} instance, for chaining.
      */
     @Override
-    @NotNull
-    public SlashCommandModel setNameLocalizations(@NotNull Map<DiscordLocale, String> nameLocalizations) {
+    public @NotNull SlashCommandModel setNameLocalizations(@NotNull Map<DiscordLocale, String> nameLocalizations) {
         this.nameLocalizations = nameLocalizations;
 
         return this;
@@ -76,42 +80,35 @@ public class SlashCommandModel extends SlashLikeCommandModel {
      * Sets the command description.
      *
      * @param description The command description.
-     * @return Current {@link SlashCommandModel} instance, for chaining.
+     * @return The {@link SlashCommandModel} instance, for chaining.
      */
-    @NotNull
-    public SlashCommandModel setDescription(@NotNull String description) {
+    public @NotNull SlashCommandModel setDescription(@NotNull String description) {
         this.description = description;
 
         return this;
     }
 
     /**
-     * Sets multiple localizations of the command description.
+     * {@inheritDoc}
      *
-     * @param descriptionLocalizations {@link Map} of {@link DiscordLocale} and description on different languages.
+     * @param descriptionLocalizations {@inheritDoc}
      * @return The {@link SlashCommandModel} instance, for chaining.
      */
     @Override
-    @NotNull
-    public SlashCommandModel setDescriptionLocalizations(@NotNull Map<DiscordLocale, String> descriptionLocalizations) {
+    public @NotNull SlashCommandModel setDescriptionLocalizations(@NotNull Map<DiscordLocale, String> descriptionLocalizations) {
         this.descriptionLocalizations = descriptionLocalizations;
 
         return this;
     }
 
     /**
-     * Sets the {@link LocalizationFunction} for this command.
-     * This allows to localize the entire command.
-     * <br>
-     * Only accepts
-     * {@link net.dv8tion.jda.api.interactions.commands.localization.ResourceBundleLocalizationFunction ResourceBundleLocalizationFunction}.
+     * {@inheritDoc}
      *
-     * @param localizationFunction The {@link LocalizationFunction}.
+     * @param localizationFunction {@inheritDoc}
      * @return The {@link SlashCommandModel} instance, for chaining.
      */
     @Override
-    @NotNull
-    public SlashCommandModel setLocalizationFunction(@NotNull LocalizationFunction localizationFunction) {
+    public @NotNull SlashCommandModel setLocalizationFunction(@NotNull LocalizationFunction localizationFunction) {
         this.localizationFunction = localizationFunction;
 
         return this;
@@ -123,9 +120,74 @@ public class SlashCommandModel extends SlashLikeCommandModel {
      * @param options The command options.
      * @return Current {@link SlashCommandModel} instance, for chaining.
      */
-    @NotNull
-    public SlashCommandModel setOptions(@NotNull List<SlashOptionData> options) {
+    public @NotNull SlashCommandModel setOptions(@NotNull List<SlashOptionData> options) {
         this.options = options;
+
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param command {@inheritDoc}
+     * @return The {@link SlashCommandModel} instance, for chaining.
+     */
+    @Override
+    public @NotNull SlashCommandModel setCommand(@NotNull BaseCommand command) {
+        this.command = command;
+
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param main {@inheritDoc}
+     * @return The {@link SlashCommandModel} instance, for chaining.
+     * @see #getMain() getMain()
+     */
+    @Override
+    public @NotNull SlashCommandModel setMain(@Nullable Method main) {
+        this.main = main;
+
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param name {@inheritDoc}
+     * @return The {@link SlashCommandModel} instance, for chaining.
+     */
+    @Override
+    public @NotNull SlashCommandModel setName(@NotNull String name) {
+        this.name = name;
+
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param guildOnly {@inheritDoc}
+     * @return The {@link SlashCommandModel} instance, for chaining.
+     */
+    @Override
+    public @NotNull SlashCommandModel setGuildOnly(boolean guildOnly) {
+        this.guildOnly = guildOnly;
+
+        return this;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param nsfw {@inheritDoc}
+     * @return The {@link SlashCommandModel} instance, for chaining.
+     */
+    @Override
+    public @NotNull SlashCommandModel setNSFW(boolean nsfw) {
+        this.nsfw = nsfw;
 
         return this;
     }
